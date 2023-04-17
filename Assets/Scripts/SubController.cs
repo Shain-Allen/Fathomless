94cf8634f600beab673eed6ee6a87be589f03d6a
joshuah abens <s-joshuah.abens@lwtech.edu>
2023-04-17 12:49:01 -0800

    The start to the sub controls. It's pretty rought now, but will be improved upon the more we work on the project.
    [git-p4: depot-paths = "//Khat/": change = 1785]

---
+++ Assets/Scripts/SubController.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubController : MonoBehaviour
+{
+
+    public float speed;
+    public float minSpeed;
+    public float maxSpeed;
+
+    public float xRotSpeed;
+    public float yRotSpeed;
+
+    public float decentSpeed;
+    public float minDecentSpeed;
+    public float maxDecentSpeed;
+
+    public Rigidbody subRigi;
+
+    void FixedUpdate()//this will use simple keycodes for now, but we can use this for the unity input system if we want. This is just to see the best way to control the sub
+    {
+        //this will ensure that the sub will allways move forward. 0 speed will stop the throttle of the speed.
+        subRigi.AddForce(transform.forward * speed);
+
+        //float rotationX = Input.GetAxis("Vertical") * yRotSpeed;
+        float rotationY = Input.GetAxis("Horizontal") * xRotSpeed;
+        Quaternion rotation = Quaternion.Euler(0.0f, rotationY, 0.0f);
+        subRigi.MoveRotation(subRigi.rotation * rotation);
+
+
+        //This will increase the speed of sub
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed += .05f;
+        }
+
+        //This will decrease the speed of sub
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            speed -= .05f;
+        }
+
+        //Moves sub up
+        if (Input.GetKey(KeyCode.W))
+        {
+            subRigi.AddForce(transform.up * decentSpeed);
+        }
+
+        //moves sub down
+        if (Input.GetKey(KeyCode.S))
+        {
+            subRigi.AddForce(-transform.up * decentSpeed);
+        }
+
+        //this will cap the speed to the set max speed
+        if (speed >= maxSpeed)
+        {
+            speed =maxSpeed;
+        }
+
+        //This will set the sub speed to zero when the thrust is tunred off
+        if (speed <= minSpeed)
+        {
+
+            //right now the sub will stop immediatly, but we can probably find a way to make it gradually stop.
+            speed = minSpeed;
+            subRigi.velocity = Vector3.zero;
+            subRigi.angularVelocity = Vector3.zero;
+        }
+
+        //this will cap the decent speed to the set max decent speed
+        if (decentSpeed >= maxDecentSpeed)
+        {
+            decentSpeed = maxDecentSpeed;
+        }
+
+        //This will set the sub decent speed to zero when the thrust is tunred off
+        if (decentSpeed <= minDecentSpeed)
+        {
+
+            //right now the sub will stop immediatly, but we can probably find a way to make it gradually stop.
+            decentSpeed = minDecentSpeed;
+            subRigi.velocity = Vector3.zero;
+            subRigi.angularVelocity = Vector3.zero;
+        }
+
+
+    }
+}
