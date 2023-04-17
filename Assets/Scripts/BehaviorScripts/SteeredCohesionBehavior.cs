a626bd09e66b30b2e321d522971efc1d72dcbc2c
maverick wilkinson <s-maverick.wilkinson@lwtech.edu>
2023-04-16 23:04:27 -0800

    Made improvements to the cohesion boid system with the SteeredCohesionBehavior system, and made a behavior for combining the rules into one collective move. the boids work! but need fine tuning.
    [git-p4: depot-paths = "//Khat/": change = 1784]

---
+++ Assets/Scripts/BehaviorScripts/SteeredCohesionBehavior.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Boid/Behavior/SteeredCohesion")]
+public class SteeredCohesionBehavior : BoidBehavior
+{
+    Vector3 currentVelocity;
+    public float agentSmoothTime = 0.5f;
+    public override Vector3 CalculateMove(BoidAgent agent, List<Transform> context, Boid boid)
+    {
+        //if no neighbors, return no adjustment
+        if (context.Count == 0)
+        {
+            return Vector3.zero;
+        }
+        Vector3 cohesionMove = Vector3.zero;
+        foreach (Transform item in context)
+        {
+            cohesionMove += item.position;
+        }
+        cohesionMove /= context.Count;
+
+        //create offset from agent position
+        cohesionMove -= agent.transform.position;
+        cohesionMove = Vector3.SmoothDamp(agent.transform.forward, cohesionMove, ref currentVelocity, agentSmoothTime);
+        return cohesionMove;
+    }
+}
