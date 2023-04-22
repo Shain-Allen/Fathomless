b9d10a45a2bc9630c66a6312b57cbb2f06630c1f
maverick wilkinson <s-maverick.wilkinson@lwtech.edu>
2023-04-21 19:56:49 -0800

    Added resource pickup functionality, and improved underwater enemy AI significantly. Now has state machine and several states.
    [git-p4: depot-paths = "//Khat/": change = 1811]

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject player;
+    public int Scrap;
+
+    private void Start()
+    {
+        Scrap = 0;
+    }
+}
