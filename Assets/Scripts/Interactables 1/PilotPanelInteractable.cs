e8f393c5ad232c8a842072bf8307b758b08bd01e
joshuah abens <s-joshuah.abens@lwtech.edu>
2023-04-24 12:33:42 -0800

    setting up player to sub controls
    [git-p4: depot-paths = "//Khat/": change = 1838]

--- Assets/Scripts/Interactables 1/PilotPanelInteractable.cs
+++ Assets/Scripts/Interactables 1/PilotPanelInteractable.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class PilotPanelInteractable : MonoBehaviour, IInteractable
 {
+    public GameObject playerLockLocation;
+    public GameObject player;
+    public SubController subScript;
+    public GameObject subCam;
+         
+
     public void Interact(GameObject player)
     {
         print("Engaged pilot controls");
         //TODO: add logic for changing to sub pilot controls
+
+        player.SetActive(false);
+        player.transform.position = playerLockLocation.transform.position;
+        subCam.SetActive(true);
+        subScript.isSub = true;
+
+
     }
 }
