d587a8d76232d5e2a1065ea88c1f8f8a9f17d1b3
Sean Spencer <s-sean.spencer@lwtech.edu>
2023-05-09 14:17:14 -0800

    test submission
    [git-p4: depot-paths = "//Khat/": change = 1959]

--- Assets/Scripts/playerScript2.cs
+++ Assets/Scripts/playerScript2.cs
@@ -10,14 +10,14 @@ public class playerScript2 : MonoBehaviour
     public LayerMask playerMask;
     public Transform groundCheck;
     public GameObject cam;
-    Vector3 velocity;
+    Vector3 direction;
 
     public GameObject sub;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -25,48 +25,66 @@ public class playerScript2 : MonoBehaviour
     {
         if (!Frozen) // if Frozen is false
         {
-            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, ~playerMask); //checks to see if the ground check is contacting anything except the player mask
-
-            float x = Input.GetAxis("Horizontal"); //sets up the x axis (A and D)
-            float z = Input.GetAxis("Vertical"); //sets up the Z axis (W and S)
-
-            if (x > 0) //pressed D
-            {
-                velocity += playerBody.transform.right;
-            }
-            if (x < 0) //pressed A
-            {
-                velocity += -playerBody.transform.right;
-            }
-            if (z > 0) //pressed W
-            {
-                velocity += playerBody.transform.forward;
-            }
-            if (z < 0) //pressed S
-            {
-                velocity += -playerBody.transform.forward;
-            }
-            
-            velocity.Normalize();
-            velocity *= Time.deltaTime * speed;
-            
-            if (inSub) //gravity if the player is in the sub, and set player to be child of sub
-            {
-                transform.SetParent(sub.transform);
-                velocity.y -= gravity * Time.deltaTime;
-            }
-            else //gravity otherwise, and remove parent
+            if (!inSub)
             {
                 transform.SetParent(null);
-                velocity.y -= gravity * Time.deltaTime / 2;
-            }
+                
+                isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, ~playerMask); //checks to see if the ground check is contacting anything except the player mask
 
-            if (isGrounded && Input.GetButtonDown("Jump")) //if you press space while grounded
-            {
-                velocity.y += jumpHeight;
+                if (Input.GetKey(KeyCode.D)) //pressed D
+                {
+                    direction += playerBody.transform.right;
+                }
+                if (Input.GetKey(KeyCode.A)) //pressed A
+                {
+                    direction += -playerBody.transform.right;
+                }
+                if (Input.GetKey(KeyCode.W)) //pressed W
+                {
+                    direction += playerBody.transform.forward;
+                }
+                if (Input.GetKey(KeyCode.S)) //pressed S
+                {
+                    direction += -playerBody.transform.forward;
+                }
+
+                direction.Normalize();
+                direction *= Time.deltaTime * speed;                          
+                              
+                direction.y -= gravity * Time.deltaTime / (10 / 3);
+                
+                if (isGrounded && Input.GetButtonDown("Jump")) //if you press space while grounded
+                {
+                    direction.y += jumpHeight;
+                }
+                //print(velocity);
+                playerBody.AddForce(direction, ForceMode.Impulse); //applies impulse force to all movements
             }
-            print(velocity);
-            playerBody.AddForce(velocity, ForceMode.Impulse); //applies impulse force to all movements
+            else
+            {
+                transform.SetParent(sub.transform);
+
+                if (Input.GetKey(KeyCode.D)) //pressed D
+                {
+                    direction += playerBody.transform.right;
+                }
+                if (Input.GetKey(KeyCode.A)) //pressed A
+                {
+                    direction += -playerBody.transform.right;
+                }
+                if (Input.GetKey(KeyCode.W)) //pressed W
+                {
+                    direction += playerBody.transform.forward;
+                }
+                if (Input.GetKey(KeyCode.S)) //pressed S
+                {
+                    direction += -playerBody.transform.forward;
+                }
+                
+                direction *= Time.deltaTime * (speed / 5);
+                transform.localPosition += new Vector3(direction.x, direction.y, direction.z); //applies impulse force to all movements
+            }
+            
         }
         else //if Frozen is true
         {
