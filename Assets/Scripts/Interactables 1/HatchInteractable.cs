794478ff58fbbb16c412fb708f5a0ffb1a23c7a1
Sean Spencer <s-sean.spencer@lwtech.edu>
2023-05-09 14:37:03 -0800

    another test submission
    [git-p4: depot-paths = "//Khat/": change = 1967]

--- Assets/Scripts/Interactables 1/HatchInteractable.cs
+++ Assets/Scripts/Interactables 1/HatchInteractable.cs
@@ -4,9 +4,30 @@ using UnityEngine;
 
 public class HatchInteractable : MonoBehaviour, IInteractable
 {
+    playerScript2 playerScript2;
+    public LayerMask seaMask, subMask;
+    public GameObject teleporter;
+    public GameObject player;
+
+    void Awake()
+    {
+        playerScript2 = player.GetComponent<playerScript2>();
+    }
+
     public void Interact(GameObject player)
     {
-        print("Interacted with the Sub Hatch");
+        playerScript2.inSub = !playerScript2.inSub;
+        StartCoroutine("Teleport");
         //TODO: add logic for moving player outside sub
+        //player.transform.position = tP.transform.position;
+    }
+
+    IEnumerator Teleport()
+    {
+        playerScript2.Frozen = true; //removes player control...
+        yield return new WaitForSeconds(0.01f); //for this long
+        playerScript2.transform.position = teleporter.transform.position; //takes player to this position
+        yield return new WaitForSeconds(0.01f); //waits this long...
+        playerScript2.Frozen = false; //to restore player control
     }
 }
