ce1fd1f445c1462284f9b7ebad345bde7e986309
maverick wilkinson <s-maverick.wilkinson@lwtech.edu>
2023-05-09 19:11:03 -0800

    implemented ellipse based character confinement system so we can make the sub walls without relying on the physics system for both. Created scene for showcasing this. Also added a gizmo function that shows the elipse in the scene view in real time, and move many of the gameplay interactable prefabs into their own folder, so we wont have to search through all of the art assets for them.
    [git-p4: depot-paths = "//Khat/": change = 1976]

--- Assets/Scripts/playerScript2.cs
+++ Assets/Scripts/playerScript2.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class playerScript2 : MonoBehaviour
@@ -11,19 +9,25 @@ public class playerScript2 : MonoBehaviour
     public Transform groundCheck;
     public GameObject cam;
     Vector3 direction;
-    public float playerHeightOffset;
+    float playerHeightOffset;
 
     public GameObject sub;
-    
+
+    public GameObject playerContainer;
+    Vector3 initialPos;
+    public float spaceRadiusX;
+    public float spaceRadiusZ;
+
     // Start is called before the first frame update
     void Start()
     {
-
+         playerHeightOffset = playerContainer.transform.localPosition.y;
     }
 
     // Update is called once per frame
     void Update()
     {
+        initialPos = playerContainer.transform.position;
         if (!Frozen) // if Frozen is false
         {
             if (!inSub)
@@ -31,7 +35,7 @@ public class playerScript2 : MonoBehaviour
                 //sets the freeze position constrains, since we're moving with transform. needed for rigidbody to work.
                 playerBody.constraints = RigidbodyConstraints.None | RigidbodyConstraints.FreezeRotation;
                 transform.SetParent(null);
-                
+
                 isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, ~playerMask); //checks to see if the ground check is contacting anything except the player mask
 
                 if (Input.GetKey(KeyCode.D)) //pressed D
@@ -52,10 +56,10 @@ public class playerScript2 : MonoBehaviour
                 }
 
                 direction.Normalize();
-                direction *= Time.deltaTime * aquaSpeed;                          
-                              
+                direction *= Time.deltaTime * aquaSpeed;
+
                 direction.y -= gravity * Time.deltaTime / (10 / 3);
-                
+
                 if (isGrounded && Input.GetButtonDown("Jump")) //if you press space while grounded
                 {
                     direction.y += jumpHeight;
@@ -85,17 +89,71 @@ public class playerScript2 : MonoBehaviour
                 {
                     direction += -playerBody.transform.forward;
                 }
-                
                 direction *= Time.deltaTime * (tereSpeed / 5);
-                print(direction);
-                transform.localPosition = new Vector3(transform.localPosition.x, playerHeightOffset, transform.localPosition.z);
-                transform.position += new Vector3(direction.x, transform.localPosition.y, direction.z); //applies impulse force to all movements
+
+                //establishes ellipse which represents player movement space
+                Vector3 newPos = transform.position + direction;
+                Vector3 offset = newPos - initialPos;
+                offset.x /= spaceRadiusX;
+                offset.z /= spaceRadiusZ;
+                //compare distance of player to origin of ellipse to see if player would be out of bounds. if so, then skip adding movement.
+                if (offset.magnitude < 1.0)
+                {
+                    transform.localPosition = new Vector3(transform.localPosition.x, playerHeightOffset, transform.localPosition.z);
+                    transform.position += new Vector3(direction.x, 0f, direction.z);
+                }
             }
-            
+
         }
         else //if Frozen is true
         {
 
         }
+    }
+
+    //Draws the ellipse to the scene view
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireMesh(CreateEllipseMesh(), initialPos);
+        Gizmos.matrix = Matrix4x4.identity;
+    }
+    private Mesh CreateEllipseMesh()
+    {
+        int numSegments = 32;
+        float angleStep = 360.0f / numSegments;
+
+        Vector3[] vertices = new Vector3[numSegments + 1];
+        Vector3[] normals = new Vector3[numSegments + 1];
+        int[] indices = new int[numSegments * 2];
+
+        vertices[0] = Vector3.zero;
+        normals[0] = Vector3.up;
+        for (int i = 0; i < numSegments; i++)
+        {
+            float angle = i * angleStep;
+            float x = Mathf.Cos(angle * Mathf.Deg2Rad);
+            float z = Mathf.Sin(angle * Mathf.Deg2Rad);
+            vertices[i + 1] = new Vector3(x * spaceRadiusX, 0.0f, z * spaceRadiusZ);
+            normals[i + 1] = Vector3.up;
+
+            if (i < numSegments - 1)
+            {
+                indices[i * 2] = i;
+                indices[i * 2 + 1] = i + 1;
+            }
+            else
+            {
+                indices[i * 2] = i;
+                indices[i * 2 + 1] = 0;
+            }
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.normals = normals;
+        mesh.SetIndices(indices, MeshTopology.Lines, 0);
+
+        return mesh;
     }
 }
