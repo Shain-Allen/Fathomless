c0e92cc0154a47727b42d91f5fb8f5ddecc1ac99
hugo autio <s-hugo.autio@lwtech.edu>
2023-05-17 16:08:47 -0800

    Art Assets
    [git-p4: depot-paths = "//Khat/": change = 2087]

---
+++ Assets/Scripts/AMCollisionDetector.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AMCollisionDetector : MonoBehaviour
+{
+    public GameObject AnimationManager;
+    public bool HasCollided = false;
+    public bool ToggleType = false;
+    void OnCollisionEnter(Collision collision)
+    {
+        if (HasCollided == false)
+        {
+            print(collision.gameObject);
+            if (ToggleType == false)
+            {
+                AnimationManager.GetComponent<AnimationToggleManager>().StartAnimation();
+                print("Animation Started");
+                HasCollided = true;
+            }
+            else if (ToggleType == true)
+            {
+                AnimationManager.GetComponent<AnimationToggleManager>().EndAnimation();
+                print("Animation Ended");
+                HasCollided = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/AnimationToggleManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationToggleManager : MonoBehaviour
+{
+    public bool Animation_Started = false;
+    public bool Animation_Completed = false;
+
+    public void StartAnimation()
+    {
+
+    }
+
+    public void EndAnimation()
+    {
+
+    }
+}
