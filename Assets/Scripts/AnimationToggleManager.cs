bf66ac1fc0fb67d2fdadf2594a11dc8746129dcd
hugo autio <s-hugo.autio@lwtech.edu>
2023-05-24 13:47:06 -0800

    Animation Proof of Concept
    [git-p4: depot-paths = "//Khat/": change = 2366]

--- Assets/Scripts/AnimationToggleManager.cs
+++ Assets/Scripts/AnimationToggleManager.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class AnimationToggleManager : MonoBehaviour
 {
+    public GameObject Submarine;
     public bool Animation_Started = false;
     public bool Animation_Completed = false;
 
     public void StartAnimation()
     {
-
+        Submarine.GetComponent<SubController>().isSub = false;
+        Submarine.GetComponent<Animator>().SetBool("Animate?", true);
+        Debug.Log("Starting Animation");
     }
 
     public void EndAnimation()
     {
-
+        Submarine.GetComponent<SubController>().isSub = false;
     }
 }
