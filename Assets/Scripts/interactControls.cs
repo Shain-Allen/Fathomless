e8b3b16c0b56b5785062e5b0d65637c7f38bc68b
joshuah abens <s-joshuah.abens@lwtech.edu>
2023-05-29 12:34:07 -0800

    More work on the sub controls
    [git-p4: depot-paths = "//Khat/": change = 2464]

--- Assets/Scripts/interactControls.cs
+++ Assets/Scripts/interactControls.cs
@@ -6,16 +6,18 @@ public class interactControls : MonoBehaviour
 {
     public Camera MainCamera;
     private RaycastHit raycast;
-    private float raycastRange = 10f;
+    private float raycastRange = 1000f;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
+            Debug.Log("Button Press");
             if (Physics.Raycast(MainCamera.transform.position, MainCamera.transform.forward, out raycast, raycastRange))
             {
                 raycast.collider.gameObject.GetComponent<IInteractable>()?.Interact(gameObject);
+                Debug.Log(raycast.collider.gameObject);
             }
         }
     }
