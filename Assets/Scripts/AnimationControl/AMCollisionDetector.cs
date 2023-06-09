7f8558c4c91989af2a72dd2a42cd8a83fcf9c6d5
maverick wilkinson <s-maverick.wilkinson@lwtech.edu>
2023-06-09 10:36:03 -0800

    Re-Added world boundaries and added a collider that selectively blocks the player from jumping off the cliff, but not the sub.
    [git-p4: depot-paths = "//Khat/": change = 3007]

--- Assets/Scripts/AnimationControl/AMCollisionDetector.cs
+++ Assets/Scripts/AnimationControl/AMCollisionDetector.cs
@@ -8,24 +8,31 @@ public class AMCollisionDetector : MonoBehaviour
     public bool HasCollided = false;
     public string AnimationName = "";
     public bool EndAnimation = false;
-    void OnTriggerEnter(Collider collision)
+    void OnCollisionEnter(Collision collision)
     {
-        if (HasCollided == false)
+        if (collision.gameObject.CompareTag("SubTag"))
         {
-            Debug.Log(collision.gameObject);
-            if (EndAnimation == false)
+            if (HasCollided == false)
             {
-                AnimationManager.GetComponent<AnimationToggleManager>().StartAnimation(AnimationName);
-                print("Animation Started");
-                HasCollided = true;
-            }
-            else if (EndAnimation == true)
-            {
-                AnimationManager.GetComponent<AnimationToggleManager>().EndAnimation();
-                print("Animation Ended");
-                HasCollided = true;
+                Debug.Log(collision.gameObject);
+                if (EndAnimation == false)
+                {
+                    AnimationManager.GetComponent<AnimationToggleManager>().StartAnimation(AnimationName);
+                    print("Animation Started");
+                    HasCollided = true;
+                }
+                else if (EndAnimation == true)
+                {
+                    AnimationManager.GetComponent<AnimationToggleManager>().EndAnimation();
+                    print("Animation Ended");
+                    HasCollided = true;
 
+                }
             }
+        }
+        else if (collision.gameObject.CompareTag("Player"))
+        {
+            CanvasController.Instance.DisplayText("I'm not getting any closer to this cliff without my sub...");
         }
     }
 }
