f26cb3e6751982e2a4fc9f6dacbc7241781e5cf9
maverick wilkinson <s-maverick.wilkinson@lwtech.edu>
2023-06-14 00:14:05 -0800

    Added credits, and wiping my hands clean.
    [git-p4: depot-paths = "//Khat/": change = 3256]

--- Assets/CreditsRoll.cs
+++ Assets/CreditsRoll.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,7 +9,44 @@ public class CreditsRoll : MonoBehaviour
     {
         credits = new string[]
         {
-            "Thank you for playing our Fathomless demo."
+            "Thank you for playing our Fathomless demo.",
+            "This couldn't be possible without our fantastic team:",
+            "Producers:",
+            "Maverick Wilkinson     Sean Spencer     Emily Adams     Ethan Berning.",
+            "Modeling and Texture artists:",
+            "Carlos Flores-Torres     Ita Cummins    Howard Chan        Micheal MCGOWAN",
+            "William Collins          An Ta           Daniel Agcaoili    Anna Wylie",
+            "Ethan Berning",
+            "Particle effects:",
+            "Tyler Bader Ramos",
+            "Lighting and Volumetrics:",
+            "Tyler Charnholm",
+            "Programmers:",
+            "Maverick Wilkinson     Joshua Abens     Sean Spencer     Hugo Autio",
+            "Animation and Rigging:",
+            "Erin Hiliker    Ita Cummins",
+            "Environment and Level Design",
+            "Emily Adams     Alex Turner",
+            "Moral support (and special thanks)",
+            "Larry Paolicelli",
+            "and the folks who made all these sounds.",
+            "Metal Breaking: Sound Finder on Youtube",
+            "Sonar Ping: Pixabay",
+            "Distress Signal 2: Pixabay",
+            "Moving Low Frequencies: Pixabay",
+            "Baleines: Pixabay",
+            "Sea and Seagull, wave: Pixabay",
+            "WalthamstowResevoire: Pixabay",
+            "muvibeat1_130BPM: yokim Pixabay",
+            "Reload: UNIVERSFIELD Pixabay",
+            "Deep Sea Ambience: Pixabay",
+            "Tank reloading: iwanPlays on Youtube",
+            "Under water sounds while scuba diving: Pixabay",
+            "SPLASH (by blaukreuz): Pixabay",
+            "underwater-crab-like-monster-growl: Pixabay",
+            "Additional sound effects from https://www.zapsplat.com",
+            "And thank you for playing.",
+            "We really hope you enjoyed the experience."
         };
 
         CanvasController.Instance.DisplayMoreText(credits, 2.5f);
@@ -20,5 +56,11 @@ public class CreditsRoll : MonoBehaviour
     {
         yield return new WaitForSeconds(credits.Length * 2.5f + 3);
         SceneManager.LoadScene("MainMenu");
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            SceneManager.LoadScene("MainMenu");
+        }
     }
 }
