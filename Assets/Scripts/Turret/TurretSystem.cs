6febbce7aba4720a012ab0dc65b86dc9bce00c10
Shain Allen <s-shain.allen@lwtech.edu>
2023-07-08 20:03:18 -0800

    Moved Turret Controls fully over to new input system
    [git-p4: depot-paths = "//Khat/": change = 3303]

--- Assets/Scripts/Turret/TurretSystem.cs
+++ Assets/Scripts/Turret/TurretSystem.cs
@@ -1,16 +1,12 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
-using System;
-using UnityEngine.Serialization;
+using UnityEngine.InputSystem;
 
 public class TurretSystem : MonoBehaviour
 {
     public float mouseSensitivity = 100f;
-    [FormerlySerializedAs("camera")] public Camera turretCamera;
-    float x_rotation = 0f;
-    float y_rotation = 0f;
+    float x_rotation;
+    float y_rotation;
     public GameObject turretProjectilePrefab;
     public Transform turretBarrel;
     public float fireDelay = 1.0f;
@@ -25,12 +21,9 @@ public class TurretSystem : MonoBehaviour
         if (isTurret)
         {
             VisualHarpoon();
-            TurretControl();
-            UpdateCamera();
         }
     }
-
-
+    
     void VisualHarpoon()
     {
         if (Time.time > nextShot)
@@ -42,26 +35,27 @@ public class TurretSystem : MonoBehaviour
             VisHarpoon.SetActive(false);
         }
     }
-    void TurretControl()
+    
+    private void OnLook(InputValue inputValue)
     {
-        if (Input.GetMouseButton(0) && Time.time > nextShot)
+        Vector2 mouse = inputValue.Get<Vector2>() * mouseSensitivity * Time.deltaTime;
+        //float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        x_rotation += mouse.x;
+        y_rotation += mouse.y;
+        x_rotation = Mathf.Clamp(x_rotation, -90f, 90f);
+        y_rotation = Mathf.Clamp(y_rotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(y_rotation, x_rotation, 0);
+    }
+    
+    private void OnFire(InputValue inputValue)
+    {
+        if (Time.time > nextShot)
         {
             FireTurret();
             turretAnimator.SetTrigger("Fire");
             StartCoroutine(ReloadTimer());
             nextShot = Time.time + fireDelay;
         }
-    }
-
-    void UpdateCamera()
-    {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-        x_rotation += mouseX;
-        y_rotation += mouseY;
-        x_rotation = Mathf.Clamp(x_rotation, -90f, 90f);
-        y_rotation = Mathf.Clamp(y_rotation, -90f, 90f);
-        transform.localRotation = Quaternion.Euler(y_rotation, x_rotation, 0);
     }
 
     void FireTurret()   
