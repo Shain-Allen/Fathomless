be69dcd04edb8b1362f7a83e2267e906486de6e1
Shain Allen <s-shain.allen@lwtech.edu>
2023-07-08 20:13:07 -0800

    Finalized cleanup of TurretSystem.cs
    [git-p4: depot-paths = "//Khat/": change = 3305]

--- Assets/Scripts/Turret/TurretSystem.cs
+++ Assets/Scripts/Turret/TurretSystem.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 public class TurretSystem : MonoBehaviour
 {
     public float mouseSensitivity = 100f;
+    private Vector2 rotation;
     float x_rotation;
     float y_rotation;
     public GameObject turretProjectilePrefab;
@@ -39,12 +40,10 @@ public class TurretSystem : MonoBehaviour
     private void OnLook(InputValue inputValue)
     {
         Vector2 mouse = inputValue.Get<Vector2>() * mouseSensitivity * Time.deltaTime;
-        //float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-        x_rotation += mouse.x;
-        y_rotation += mouse.y;
-        x_rotation = Mathf.Clamp(x_rotation, -90f, 90f);
-        y_rotation = Mathf.Clamp(y_rotation, -90f, 90f);
-        transform.localRotation = Quaternion.Euler(y_rotation, x_rotation, 0);
+        rotation += mouse;
+        rotation.x = Mathf.Clamp(rotation.x, -90f, 90f);
+        rotation.y = Mathf.Clamp(rotation.y, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(rotation.y, rotation.x, 0);
     }
     
     private void OnFire(InputValue inputValue)
