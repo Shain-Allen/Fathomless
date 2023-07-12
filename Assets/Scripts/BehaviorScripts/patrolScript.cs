1344eb6b6194ece5ed41db94eb3ce91734b82f8f
joshuah abens <s-joshuah.abens@lwtech.edu>
2023-07-12 13:49:32 -0800

    added more systems into the enemy's AI
    [git-p4: depot-paths = "//Khat/": change = 3327]
    
    
    Former-commit-id: ab85a332ef410faba575ee9cfd04270e5445d736

--- Assets/Scripts/BehaviorScripts/patrolScript.cs
+++ Assets/Scripts/BehaviorScripts/patrolScript.cs
@@ -15,6 +15,7 @@ public class patrolScript : MonoBehaviour
     public float startWaitTime;
 
     float distance;
+    float patrolDistance;
 
     public bool canAttack;
     public bool canMove;
@@ -23,10 +24,16 @@ public class patrolScript : MonoBehaviour
 
     public bool canFollowPlayer;
     public bool randomPatrol;
+
+    public GameObject fish;
+
+    public float fixedRotation;
+    public bool takenDamage;
     // Start is called before the first frame update
     void Start()
     {
         pos = GameObject.FindGameObjectsWithTag("patrolPoint");
+        patrolCase = 1;
     }
 
     // Update is called once per frame
@@ -37,33 +44,45 @@ public class patrolScript : MonoBehaviour
         Debug.Log(patrolCase);
         distance = Vector3.Distance(transform.position, player.transform.position);
 
-        if(distance >= 15)
+
+        if(distance >= 20)
         {
             patrolCase = 1;
         }
 
         if (canFollowPlayer)
         {
-            if (distance <= 10 && distance >= 3)
+            if (distance <= 15 && distance >= 8)
             {
                 patrolCase = 2;
             }
 
-            if(distance <= 3)
+            if(distance <= 7)
             {
                 patrolCase = 3;
             }
+            if (distance <= 2)
+            {
+                patrolCase = 4;
+            }
+        }
+
+        if(takenDamage)
+        {
+            patrolCase = 5;
         }
 
         switch (patrolCase)
         {
             case 1:
                 canMove = true;
-                //transform.LookAt(pos[currentPos].transform.position);\
+                speed = 1f;
+                transform.LookAt(pos[currentPos].transform.position);
                 Patrol();
                 break;
             case 2:
                 canMove = false;
+                agent.speed = 10f;
                 transform.LookAt(player.transform.position);
                 agent.SetDestination(player.transform.position);
                 agent.isStopped = false;
@@ -72,6 +91,23 @@ public class patrolScript : MonoBehaviour
                 transform.LookAt(player.transform.position);
                 canMove = false;
                 agent.isStopped = true;
+                break;
+            case 4:
+
+                break;
+            case 5:
+                patrolDistance = Vector3.Distance(transform.position, pos[currentPos].transform.position);
+                canMove = true;
+                transform.LookAt(pos[currentPos].transform.position);
+                agent.SetDestination(pos[currentPos].transform.position);
+                speed = 15f;
+                agent.isStopped = false;
+
+                if (patrolDistance <= 5) 
+                {
+                    patrolCase = 1;
+                    takenDamage = false;   
+                }
                 break;
         }
     }
