742b98688b49c278489a6ce3f3d1e11b3ad7810f
Shain Allen <s-shain.allen@lwtech.edu>
2023-07-21 15:05:02 -0800

    Fixed a few UI issues.
    	-Sub Dash now has proper In world UI allow for the health bar to actually work
    	-Player UI anchor points didn't get the updates made on the prefab.
    
    additionally the Sub also now has form fitting Colliders
    
    [git-p4: depot-paths = "//Khat/": change = 3406]
    
    
    Former-commit-id: b66cb0278d42165dca4e98a90c626790ce5b2ed8

--- Assets/Scripts/UI/HealthScrapBarManager.cs
+++ Assets/Scripts/UI/HealthScrapBarManager.cs
@@ -1,26 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthScrapBarManager : MonoBehaviour
 {
-    public Text text;
-    Vector3 newScale;
+    [SerializeField] private TextMeshProUGUI scrapText;
+    [SerializeField] private Slider healthBar;
+    
     private void FixedUpdate()
     {
-        if(GameManager.Instance.SubHealth <= 0)
-        {
-            newScale = new Vector3(0, 1, 1);
-            gameObject.transform.localScale = newScale;
-        }
-        else
-        {
-            float scale = (-1f / (float)(SubDamageManager.instance.damagePoint.Length)) * (float)GameManager.Instance.SubHealth;
-            newScale = new Vector3(scale, 1, 1);
-            gameObject.transform.localScale = newScale;
-        }
+        // if(GameManager.Instance.SubHealth <= 0)
+        // {
+        //     newScale = new Vector3(0, 1, 1);
+        //     gameObject.transform.localScale = newScale;
+        // }
+        // else
+        // {
+        //     float scale = (-1f / (float)(SubDamageManager.instance.damagePoint.Length)) * (float)GameManager.Instance.SubHealth;
+        //     newScale = new Vector3(scale, 1, 1);
+        //     gameObject.transform.localScale = newScale;
+        // }
 
-        text.text = GameManager.Instance.Scrap.ToString(); ;
+        healthBar.value = Convert.ToSingle(GameManager.Instance.SubHealth / SubDamageManager.Instance.damagePoint.Length);
+
+        scrapText.text = GameManager.Instance.Scrap.ToString(); ;
+    }
+
+    public void blank()
+    {
+        
     }
 }
