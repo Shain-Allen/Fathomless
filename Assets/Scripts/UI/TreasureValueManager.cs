7f613a360fb3c09378a62d79e926267bc8873874
Shain Allen <s-shain.allen@lwtech.edu>
2023-07-20 18:06:39 -0800

    Updated Treasure Value to allow for control of the transfer speed of collected Treasure/Gold
    [git-p4: depot-paths = "//Khat/": change = 3403]
    
    
    Former-commit-id: 5c05f5c71370ade2236391296f1aaf4a543ce435

--- Assets/Scripts/UI/TreasureValueManager.cs
+++ Assets/Scripts/UI/TreasureValueManager.cs
@@ -1,23 +1,29 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class TreasureValueManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI treasureText;
+    [SerializeField] private uint transferSpeed = 1;
 
     private void Update()
     {
-        if (GameManager.gminstance.newTreasure > 0 && GameManager.gminstance.currentTreasure <= GameManager.gminstance.treasureMax)
+        if (GameManager.gminstance.newTreasure >= transferSpeed && GameManager.gminstance.currentTreasure <= GameManager.gminstance.treasureMax)
         {
-            AddTreasure();
+            AddTreasure(transferSpeed);
+        }
+        else if (GameManager.gminstance.newTreasure > 0 && GameManager.gminstance.currentTreasure <= GameManager.gminstance.treasureMax)
+        {
+            AddTreasure(1);
         }
     }
 
-    private void AddTreasure()
+    private void AddTreasure(uint transferValue)
     {
-        GameManager.gminstance.currentTreasure++;
-        GameManager.gminstance.newTreasure--;
+        GameManager.gminstance.currentTreasure += transferValue;
+        GameManager.gminstance.newTreasure -= transferValue;
 
         treasureText.text = GameManager.gminstance.currentTreasure.ToString().PadLeft(7, '0');
     }
