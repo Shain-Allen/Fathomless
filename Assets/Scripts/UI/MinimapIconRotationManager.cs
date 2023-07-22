d9820b24009b9e24e56747b454b02c72cf3fa155
Shain Allen <s-shain.allen@lwtech.edu>
2023-07-22 13:59:50 -0800

    Made it so that the RenderImage file is no longer Generated over and over causing p4v to add it automaticlly
    [git-p4: depot-paths = "//Khat/": change = 3413]
    
    
    Former-commit-id: 2ccca5bf74e922407d84c3cc78c4171f9e2e412c

--- Assets/Scripts/UI/MinimapIconRotationManager.cs
+++ Assets/Scripts/UI/MinimapIconRotationManager.cs
@@ -1,16 +1,24 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MinimapIconRotationManager : MonoBehaviour
 {
     public LayerMask targetLayer;
+    [SerializeField] private int textureWidth = 512;
+    [SerializeField] private int textureHight = 512;
+    [SerializeField] private RenderTextureFormat textureFormat = RenderTextureFormat.ARGB32;
+    [SerializeField] private FilterMode filterMode = FilterMode.Bilinear;
+
+    [SerializeField] private RawImage mapUI;
+
+    private RenderTexture renderTexture;
 
     private List<Transform> objectsWithLayer = new List<Transform>();
     private void Awake()
     {
         FindIcons();
+        GenerateRenderTexture();
     }
 
     private void FindIcons()
@@ -34,6 +42,21 @@ public class MinimapIconRotationManager : MonoBehaviour
             Vector3 spin = new Vector3(90, 0, minimapRotationY);
             Quaternion rot = Quaternion.Euler(spin);
             obj.localRotation = rot;
+        }
+    }
+
+    private void GenerateRenderTexture()
+    {
+        // Create a new RenderTexture with the specified Properties
+        renderTexture = new RenderTexture(textureWidth, textureHight, 0, textureFormat);
+        
+        // Set the Filter mode to the specified mode
+        renderTexture.filterMode = filterMode;
+
+        if (TryGetComponent(out Camera mapCam))
+        {
+            mapCam.targetTexture = renderTexture;
+            mapUI.texture = renderTexture;
         }
     }
 }
