cabc5143a3b927fb6859cb4ebbb983271e15b900
Shain Allen <s-shain.allen@lwtech.edu>
2023-07-24 19:42:57 -0800

    Fixing TreasureInteractable script being left behind
    [git-p4: depot-paths = "//Khat/": change = 3449]
    
    
    Former-commit-id: e1e39a638d3c34770b79822f27e744663c1513bb

--- Assets/Scripts/Interactables/TreasureInteractable.cs
+++ Assets/Scripts/Interactables/TreasureInteractable.cs
@@ -1,11 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
+[RequireComponent(typeof(Collider))]
 public class TreasureInteractable : MonoBehaviour, IInteractable
 {
+    [SerializeField] private uint nominalValue;
+    [SerializeField] private uint valueRange;
+
     public void Interact(GameObject player)
     {
-        throw new System.NotImplementedException();
+        float lowBound = Convert.ToSingle(nominalValue - valueRange);
+        float upperBound = (Convert.ToSingle(nominalValue + valueRange));
+        
+        GameManager.gminstance.newTreasure += Convert.ToUInt32(Random.Range(lowBound, upperBound));
+        Destroy(gameObject);
+    }
+
+    private void OnValidate()
+    {
+        if (TryGetComponent(out Collider tiCollider) && !tiCollider.isTrigger)
+        {
+            tiCollider.isTrigger = true;
+            Debug.LogWarning($"The Collider for {gameObject.name} has been changed to a trigger to allow the TreasureInteractable script to work");
+        }
     }
 }
