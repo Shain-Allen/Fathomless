c343a0acecea88ab8267382944455ab578a947fa
Shain Allen <s-shain.allen@lwtech.edu>
2023-08-09 22:05:40 -0800

    Updates to the onscreen UI and Map system. the scrap counter is not implimented yet. to adjust the scale of the Map you must change the Bounds Size parameter on the bounds child object of the MiniMapWorldBounds Object
    [git-p4: depot-paths = "//Khat/": change = 3606]
    
    
    Former-commit-id: 042255ee66c66bf8f50bd5a8011d5a7a18227474

--- Assets/Scripts/UI/Map/Bounds.cs
+++ Assets/Scripts/UI/Map/Bounds.cs
@@ -1,9 +1,24 @@
+using System;
 using UnityEngine;
 
 public class Bounds : MonoBehaviour
 {
+    [SerializeField] private Transform target;
+    [SerializeField] private float boundsSize = 25f;
+    
     [SerializeField] private Transform lower;
     [SerializeField] private Transform upper;
+
+
+    private void Awake()
+    {
+        SetBoundDist(boundsSize);
+    }
+
+    private void Update()
+    {
+        transform.parent.position = new Vector3(target.position.x, 0, target.position.z);
+    }
 
     public Vector2 FindNormalizedPosition(Vector3 position)
     {
@@ -17,5 +32,11 @@ public class Bounds : MonoBehaviour
         float zPosition = Mathf.InverseLerp(lower.position.z, upper.position.z, position.z);
 
         return new Vector2(xPosition, zPosition);
+    }
+
+    public void SetBoundDist(float dist)
+    {
+        lower.localPosition = new Vector3(-dist, 0, -dist);
+        upper.localPosition = new Vector3(dist, 0, dist);
     }
 }
