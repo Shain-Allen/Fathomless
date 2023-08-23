38daeafb71ffddc3caa783cb3566338e368ca623
Jonathan Saeteurn <s-jonathan.saeteurn@lwtech.edu>
2023-08-23 12:54:33 -0800

    shain said it works
    [git-p4: depot-paths = "//Khat/": change = 3723]
    
    
    Former-commit-id: 6d143ed81ac4672d89f6218002b634256ad67ec5

--- Assets/Scripts/UI/TreasureValueManager.cs
+++ Assets/Scripts/UI/TreasureValueManager.cs
@@ -2,21 +2,64 @@ using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Serialization;
+using System.Collections;
 
 public class TreasureValueManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI treasureText;
     [SerializeField] private uint transferSpeed = 1;
+    private bool isUpdatingTreasure = false;
 
     private void Update()
     {
         if (GameManager.gminstance.newTreasure >= transferSpeed && GameManager.gminstance.currentTreasure <= GameManager.gminstance.treasureMax)
         {
-            AddTreasure(transferSpeed);
+            if (!isUpdatingTreasure)
+            {
+                StartUpdatingTreasure();
+            }
         }
         else if (GameManager.gminstance.newTreasure > 0 && GameManager.gminstance.currentTreasure <= GameManager.gminstance.treasureMax)
         {
+            if (!isUpdatingTreasure)
+            {
+                StartUpdatingTreasure();
+            }
+        }
+        else
+        {
+            if (isUpdatingTreasure)
+            {
+                StopUpdatingTreasure();
+            }
+        }
+    }
+
+    private void StartUpdatingTreasure()
+    {
+        isUpdatingTreasure = true;
+        StartCoroutine(UpdateTreasureCoroutine());
+        GlobalSoundsManager.instance.PlayTreasureCollect();
+    }
+
+    private void StopUpdatingTreasure()
+    {
+        isUpdatingTreasure = false;
+        GlobalSoundsManager.instance.StopTreasureCollect();
+    }
+
+    private IEnumerator UpdateTreasureCoroutine()
+    {
+        while (GameManager.gminstance.newTreasure >= transferSpeed && GameManager.gminstance.currentTreasure <= GameManager.gminstance.treasureMax)
+        {
+            AddTreasure(transferSpeed);
+            yield return null;
+        }
+
+        while (GameManager.gminstance.newTreasure > 0 && GameManager.gminstance.currentTreasure <= GameManager.gminstance.treasureMax)
+        {
             AddTreasure(1);
+            yield return null;
         }
     }
 
@@ -24,7 +67,6 @@ public class TreasureValueManager : MonoBehaviour
     {
         GameManager.gminstance.currentTreasure += transferValue;
         GameManager.gminstance.newTreasure -= transferValue;
-
         treasureText.text = GameManager.gminstance.currentTreasure.ToString().PadLeft(7, '0');
     }
 }
